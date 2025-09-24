6456ff2011ce2e81fbf082aa3712788fc0753fd4
argson <228495808+ArmanDevSil@users.noreply.github.com>
2025-09-24 18:47:34 -0300

    Add initial styles in components and delete 'react.svg'

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -3,9 +3,11 @@ import Button from '../ui/Button/Button';
 const Hero = () => {
   return (
     <>
-      <h1>Learn The Modern Way in DisMode</h1>
-      <p>Discover how to really learn in the modern age</p>
-      <Button text="Learn in DisMode" />
+      <div className="hero-text-box">
+        <h1>Learn The Modern Way in DisMode</h1>
+        <p>Discover how to really learn in the modern age</p>
+        <Button className="btn btn--cta" text="Learn in DisMode" />
+      </div>
     </>
   );
 };
--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,9 +1,22 @@
+import styles from './Button.module.css';
+
 interface ButtonProps {
   text: string;
+  className: string;
+  id?: string;
 }
 
-const Button = ({ text }: ButtonProps) => {
-  return <button>{text}</button>;
+const Button = ({ text, className, id }: ButtonProps) => {
+  const classNames = className
+    .split(' ')
+    .map(c => styles[c])
+    .join(' ');
+
+  return (
+    <button className={classNames} id={id}>
+      {text}
+    </button>
+  );
 };
 
 export default Button;
