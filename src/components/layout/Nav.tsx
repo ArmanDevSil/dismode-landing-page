10235ad933dc2bd862a3b29820eb280f277c50d0
argson <228495808+ArmanDevSil@users.noreply.github.com>
2025-09-29 10:00:16 -0300

    Add new list-item #featues and change item-list #about position

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -9,13 +9,16 @@ const Nav = () => {
             <a href="#">Home</a>
           </li>
           <li>
-            <a href="#about">About</a>
+            <a href="#features">Features</a>
           </li>
           <li>
             <a href="#pricing">Pricing</a>
           </li>
           <li>
             <a href="#testimonials">Testimonials</a>
+          </li>
+          <li>
+            <a href="#about">About Us</a>
           </li>
         </ul>
       </nav>
