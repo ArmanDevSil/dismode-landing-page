cf8a91e5f752205ed2496ddae754b1921441b7e7
argson <228495808+ArmanDevSil@users.noreply.github.com>
2025-09-29 10:50:46 -0300

    Add initial 'Hero' styles

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,9 +1,10 @@
 import Button from '../ui/Button/Button';
+import styles from './Hero.module.css';
 
 const Hero = () => {
   return (
-    <section className="section section--header">
-      <div className="hero-text-box">
+    <section className={`section ${styles.sectionHero} flex flex-center`}>
+      <div className={`${styles.heroTextBox} flex flex-center flex-column`}>
         <h1>Learn The Modern Way in DisMode</h1>
         <p>Discover how to really learn in the modern age</p>
         <Button className="btn btn--cta" text="Learn in DisMode" />
