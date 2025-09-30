d1ece644333dce3480323d3e44f98dcf7ab8e48b
argson <228495808+ArmanDevSil@users.noreply.github.com>
2025-09-30 13:59:37 -0300

    Create logos carousel in 'TrustedBy' section

--- src/components/sections/TrustedBy.tsx
+++ src/components/sections/TrustedBy.tsx
@@ -1,19 +1,32 @@
 import styles from './TrustedBy.module.css';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { library, type IconProp } from '@fortawesome/fontawesome-svg-core';
+import reactLogo from '../../assets/img/react.svg';
+import typescriptLogo from '../../assets/img/typescript.svg';
+import npmLogo from '../../assets/img/npm.svg';
+import css3Logo from '../../assets/img/css3.svg';
+import htmlLogo from '../../assets/img/html5.svg';
+import gitLogo from '../../assets/img/git.svg';
+import githubLogo from '../../assets/img/github.svg';
 
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-
-library.add(fas, far, fab);
+const logos: { [key: string]: string } = {
+  react: reactLogo,
+  typescript: typescriptLogo,
+  npm: npmLogo,
+  css3: css3Logo,
+  html: htmlLogo,
+  git: gitLogo,
+  github: githubLogo,
+};
 
 const TrustedBy = () => {
-  const brandsLogos: string[][] = [
-    ['fab', 'react'],
-    ['fab', 'css3'],
-    ['fab', 'html5'],
+  const logosNames: string[] = [
+    'react',
+    'typescript',
+    'npm',
+    'css3',
+    'html',
+    'git',
+    'github',
   ];
 
   return (
@@ -23,12 +36,16 @@ const TrustedBy = () => {
       <h2>Trusted By</h2>
       <div className={styles.logosContainer}>
         <div className={`container ${styles.logosSlider} flex flex-center`}>
-          {brandsLogos.map(brandLogo => {
+          {logosNames.map(logoName => {
+            const key: string = crypto.randomUUID();
             return (
-              <FontAwesomeIcon
-                className={styles.icon}
-                icon={brandLogo as IconProp}
-              />
+              <img src={logos[logoName]} alt={`${logoName} logo`} key={key} />
+            );
+          })}
+          {logosNames.map(logoName => {
+            const key: string = crypto.randomUUID();
+            return (
+              <img src={logos[logoName]} alt={`${logoName} logo`} key={key} />
             );
           })}
         </div>
