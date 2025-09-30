7a948df42a9b4e7ac04a14476f55aae173e36054
argson <228495808+ArmanDevSil@users.noreply.github.com>
2025-09-30 14:24:58 -0300

    Create 'Logos' ui component to return a map of the logo's images

---
+++ src/components/ui/Logos/Logos.tsx
@@ -0,0 +1,39 @@
+import reactLogo from '../../../assets/img/react.svg';
+import typescriptLogo from '../../../assets/img/typescript.svg';
+import npmLogo from '../../../assets/img/npm.svg';
+import css3Logo from '../../../assets/img/css3.svg';
+import htmlLogo from '../../../assets/img/html5.svg';
+import gitLogo from '../../../assets/img/git.svg';
+import githubLogo from '../../../assets/img/github.svg';
+
+const logos: { [key: string]: string } = {
+  react: reactLogo,
+  typescript: typescriptLogo,
+  npm: npmLogo,
+  css3: css3Logo,
+  html: htmlLogo,
+  git: gitLogo,
+  github: githubLogo,
+};
+
+const Logos = () => {
+  const logosNames: string[] = [
+    'react',
+    'typescript',
+    'npm',
+    'css3',
+    'html',
+    'git',
+    'github',
+  ];
+  return (
+    <>
+      {logosNames.map(logoName => {
+        const key: string = crypto.randomUUID();
+        return <img src={logos[logoName]} alt={`${logoName} logo`} key={key} />;
+      })}
+    </>
+  );
+};
+
+export default Logos;
