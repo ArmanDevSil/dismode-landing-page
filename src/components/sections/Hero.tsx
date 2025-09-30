8de64f8b4f057d780d2e2d6fe5a190599f42dc89
argson <228495808+ArmanDevSil@users.noreply.github.com>
2025-09-30 15:49:15 -0300

    Add new 'span' element and change'p' font-size

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -5,7 +5,9 @@ const Hero = () => {
   return (
     <section className={`section ${styles.sectionHero} flex flex-center`}>
       <div className={`${styles.heroTextBox} flex flex-center flex-column`}>
-        <h1>Learn The Modern Way in DisMode</h1>
+        <h1>
+          Learn The Modern Way in <span>DisMode</span>
+        </h1>
         <p>Discover how to really learn in the modern age</p>
         <Button className="btn btn--cta" text="Learn in DisMode" />
       </div>
