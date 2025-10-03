7e13177aa8ebdfd306fc72d13bf1a559041d4aab
argson <228495808+ArmanDevSil@users.noreply.github.com>
2025-10-03 15:44:47 -0300

    Create new data for 'HowItWorks' section

--- src/data/howItWorks.tsx
+++ src/data/howItWorks.tsx
@@ -1,8 +1,14 @@
 import type React from 'react';
 
+import roadmapImg from '../assets/img/roadmap.png';
+import mentoringImg from '../assets/img/mentoring.png';
+import guidedImg from '../assets/img/guided.png';
+import jobImg from '../assets/img/job.png';
+
 interface howItWorksStep {
   title: string;
   text: React.ReactNode | string;
+  img?: string;
 }
 
 export const howItWorksSteps: howItWorksStep[] = [
@@ -14,17 +20,35 @@ export const howItWorksSteps: howItWorksStep[] = [
         learning.
       </>
     ),
+    img: roadmapImg,
   },
   {
     title: 'Unlimited Mentoring',
-    text: 'Have unlimited mentoring all along the learning.',
+    text: (
+      <>
+        Have <strong>unlimited mentoring</strong> all along the learning.
+      </>
+    ),
+    img: mentoringImg,
   },
   {
     title: 'Guided Courses',
-    text: 'With guided courses, complete new projects to use as portfolio.',
+    text: (
+      <>
+        With <strong>guided courses</strong>, complete new projects to use as
+        portfolio.
+      </>
+    ),
+    img: guidedImg,
   },
   {
     title: 'Job Opporturnitites',
-    text: 'Our students have 80% more job opporturnitites with our partnerships',
+    text: (
+      <>
+        Our students have <strong>80%</strong> more{' '}
+        <strong>job opporturnitites</strong> with our partnerships
+      </>
+    ),
+    img: jobImg,
   },
 ];
