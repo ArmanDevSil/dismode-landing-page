106c8ba4c3111c9df183961eda9c9aabdeff360c
argson <228495808+ArmanDevSil@users.noreply.github.com>
2025-10-03 15:45:38 -0300

    Update 'HowItWorks' and add its new images

--- src/components/sections/HowItWorks.tsx
+++ src/components/sections/HowItWorks.tsx
@@ -1,14 +1,15 @@
+import styles from './HowItWorks.module.css';
 import How from '../ui/How/How';
 import { howItWorksSteps } from '../../data/howItWorks.tsx';
 
 const HowItWorks = () => {
   return (
-    <section className="section section--how" id="how">
+    <section className={`section ${styles.sectionHow}`} id="how">
       <h2>
         How to Learn in <span>DisMode</span>
       </h2>
       {howItWorksSteps.map(how => {
-        return <How title={how.title} text={how.text} />;
+        return <How title={how.title} text={how.text} img={how.img} />;
       })}
     </section>
   );
--- src/components/ui/How/How.tsx
+++ src/components/ui/How/How.tsx
@@ -3,16 +3,22 @@ import styles from './How.module.css';
 interface HowProps {
   title: string;
   text: React.ReactNode;
+  img: string;
 }
 
-const How = ({ title, text }: HowProps) => {
+const How = ({ title, text, img }: HowProps) => {
   return (
     <div className={`container ${styles.containerHow}`}>
       <div className={styles.howTextBox}>
         <h3>{title}</h3>
         <p className="textboxHow">{text}</p>
       </div>
-      <img src="" alt="RoadMap example image" className="imgHow" />
+
+      {img && (
+        <div className={`container ${styles.containerImg}`}>
+          <img src={img} alt="RoadMap example image" className="imgHow" />
+        </div>
+      )}
     </div>
   );
 };
